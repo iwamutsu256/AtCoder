589670eb7d3737e18aeab356fc42d38c314c32c4
iwamutsu256 <iwamutsu0105@gmail.com>
2026-02-09 18:33:07 +0900

    problem_02_09

--- Python/abc432/c/main.py
+++ Python/abc432/c/main.py
@@ -1,3 +1,11 @@
-# ax+by = Z, x+y = Ai
-#  y = Ai - x
-# ax+b(Ai-x) = Z
+# 小さな飴の重量はXグラム 大きな飴の重量はYグラム
+# 子供の数はN人
+# 小さな飴の個数をBi 大きな飴の個数をCiとすると、
+# Ai = Bi + Ci
+# BiX + CiY = BjX + CjY
+# Bi = Ai - Ci
+# (Ai - Ci)X + CiY = (Aj - Cj)X + CjY
+# AiX - CiX + CiY = AjX - CjX + CjY
+# AiX - AjX = CiX - CiY - CjX + CjY
+# (Ai-Aj)X = (Ci-Cj)X - (Ci-Cj)Y = (Ci-Cj)(X-Y)
+# (Ai-Aj)/(Ci-Cj) = (X-Y)/X

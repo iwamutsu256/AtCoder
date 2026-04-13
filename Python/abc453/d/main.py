ce1a2e4358b74b9a9f7c32c6944c5c95b8beec44
iwamutsu256 <iwamutsu0105@gmail.com>
2026-04-13 18:04:34 +0900

    review D

--- Python/abc453/d/main.py
+++ Python/abc453/d/main.py
@@ -2,88 +2,53 @@ from collections import deque
 
 # 幅優先探索かも
 H,W = map(int,input().split())
-S = [list(input()) for _ in range(H)]
-T = []
-direction = [None] * H*W
-start = (0,0)
-goal = (0,0)
+S = [input() for _ in range(H)]
+start = [0,0]
+goal = [0,0]
 for i in range(H):
     for j in range(W):
         if S[i][j] == "S":
-            start = (i,j)
+            start = [i,j]
         if S[i][j] == "G":
-            goal = (i,j)
+            goal = [i,j]
 
-queue = deque([start])
-visited = set()
+# 位置と前回動いた方向をセットで状態とし、状態をノードとして幅優先探索をする。
+# 状態を(y,x,direction)で定義する
+# キューには経路復元用のprev上のインデックスを持たせる
+queue = deque([(start[0],start[1],None,None)])
+visited = [[[False] * 4 for _ in range(W)] for _ in range(H)]
+prevs = []
+flag = False
+index = 0
+moves = [(1,0,"D",0),(0,1,"R",1),(-1,0,"U",2),(0,-1,"L",3)]
 while queue:
     node = queue.popleft()
-    if node == goal:
-        print("Yes")
+    prevs.append(node)
+    if node[0] == goal[0] and node[1] == goal[1]:
+        flag = True
         break
-    (y,x) = node
-    if S[y][x] == "." or S[y][x] == "S":
-        visited.add(node)
-        for (dy,dx) in range([(0,1),(1,0),(-1,0),(0,-1)]):
-            ny = y + dy
-            nx = x + dx
-            if 0 <= nx < W and 0 <= ny < H and S[ny][nx] != "#" and not (ny,nx) in visited:
-                queue.append((ny,nx))
-                if S[ny][nx] == "." or S[ny][nx] == "G" or S[ny][nx] == "o" or S[ny][nx] == "x":
-                    if (dy,dx) == (1,0):
-                        direction[ny*W+nx] = "D"
-                    elif (dy,dx) == (0,1):
-                        direction[ny*W+nx] = "R"
-                    elif (dy,dx) == (-1,0):
-                        direction[ny*W+nx] = "U"
-                    else:
-                        direction[ny*W+nx] = "L"
-    elif S[y][x] == "x":
-        for (dy,dx) in range([(0,1),(1,0),(-1,0),(0,-1)]):
-            if (dy,dx) == (0,1) and S[y][x] == "R":
-                continue
-            elif (dy,dx) == (1,0) and S[y][x] == "D":
-                continue
-            elif (dy,dx) == (-1,0) and S[y][x] == "U":
-                continue
-            elif (dy,dx) == (0,-1) and S[y][x] == "L":
-                continue
-            ny = y + dy
-            nx = x + dx
-            if 0 <= nx < W and 0 <= ny < H and S[ny][nx] != "#" and not (ny,nx) in visited:
-                queue.append((ny,nx))
-                if S[ny][nx] == "." or S[ny][nx] == "G" or S[ny][nx] == "o" or S[ny][nx] == "x":
-                    if (dy,dx) == (1,0):
-                        direction[ny*W+nx] = "D"
-                    elif (dy,dx) == (0,1):
-                        direction[ny*W+nx] = "R"
-                    elif (dy,dx) == (-1,0):
-                        direction[ny*W+nx] = "U"
-                    else:
-                        direction[ny*W+nx] = "L"
-    elif S[y][x] == "o":
-        for (dy,dx) in range([(0,1),(1,0),(-1,0),(0,-1)]):
-            if (dy,dx) != (0,1) and S[y][x] == "R":
-                continue
-            elif (dy,dx) != (1,0) and S[y][x] == "D":
-                continue
-            elif (dy,dx) != (-1,0) and S[y][x] == "U":
-                continue
-            elif (dy,dx) != (0,-1) and S[y][x] == "L":
-                continue
-            ny = y + dy
-            nx = x + dx
-            if 0 <= nx < W and 0 <= ny < H and S[ny][nx] != "#" and not (ny,nx) in visited:
-                queue.append((ny,nx))
-                if S[ny][nx] == "." or S[ny][nx] == "G" or S[ny][nx] == "o" or S[ny][nx] == "x":
-                    if (dy,dx) == (1,0):
-                        direction[ny*W+nx] = "D"
-                    elif (dy,dx) == (0,1):
-                        direction[ny*W+nx] = "R"
-                    elif (dy,dx) == (-1,0):
-                        direction[ny*W+nx] = "U"
-                    else:
-                        direction[ny*W+nx] = "L"
+    cur_y,cur_x,prev_direction,prev_index = node
+    cell = S[cur_y][cur_x]
+    for dy,dx,direction,d_idx in moves:
+        if cell == "o" and direction != prev_direction:
+            continue
+        if cell == "x" and direction == prev_direction:
+            continue
+        ny,nx = cur_y+dy,cur_x+dx
+        if 0 <= ny < H and 0 <= nx < W and S[ny][nx] != "#" and not visited[ny][nx][d_idx]:
+            queue.append((ny,nx,direction,index))
+            visited[ny][nx][d_idx] = True
+    index += 1
+
+if flag:
+    print("Yes")
+    # 経路復元
+    # prevsのindexからさかのぼる
+    reversed_path = []
+    while index != 0:
+        reversed_path.append(prevs[index][2])
+        index = prevs[index][3]
+    path = reversed(reversed_path)
+    print("".join(path))
 else:
     print("No")
-
